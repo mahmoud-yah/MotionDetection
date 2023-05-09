58dd1d12a241671a863cbebd6f42d8f5d110f639
Mahmoud Yahya <mahmoudyah.my@gmail.com>
2023-05-09 20:04:23 +0300

    Add project files.

---
+++ MotionDetection/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBoxDevice = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lowerThresholdBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.upperThresholdBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(16, 43);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(497, 393);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // comboBoxDevice
+            // 
+            this.comboBoxDevice.FormattingEnabled = true;
+            this.comboBoxDevice.Location = new System.Drawing.Point(13, 13);
+            this.comboBoxDevice.Name = "comboBoxDevice";
+            this.comboBoxDevice.Size = new System.Drawing.Size(279, 24);
+            this.comboBoxDevice.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(325, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(87, 26);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(428, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(85, 26);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // valueLabel
+            // 
+            this.valueLabel.AutoSize = true;
+            this.valueLabel.Location = new System.Drawing.Point(13, 484);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(122, 16);
+            this.valueLabel.TabIndex = 4;
+            this.valueLabel.Text = "Amount of motion: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Detection threshold between:";
+            // 
+            // lowerThresholdBox
+            // 
+            this.lowerThresholdBox.Location = new System.Drawing.Point(236, 443);
+            this.lowerThresholdBox.Name = "lowerThresholdBox";
+            this.lowerThresholdBox.Size = new System.Drawing.Size(47, 22);
+            this.lowerThresholdBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 446);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "and";
+            // 
+            // upperThresholdBox
+            // 
+            this.upperThresholdBox.Location = new System.Drawing.Point(325, 443);
+            this.upperThresholdBox.Name = "upperThresholdBox";
+            this.upperThresholdBox.Size = new System.Drawing.Size(47, 22);
+            this.upperThresholdBox.TabIndex = 8;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(396, 442);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(78, 24);
+            this.saveButton.TabIndex = 9;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 516);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.upperThresholdBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lowerThresholdBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valueLabel);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.comboBoxDevice);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "Form1";
+            this.Text = "Motion Detection";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBoxDevice;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label valueLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lowerThresholdBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox upperThresholdBox;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ MotionDetection/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+using AForge.Vision.Motion;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection fic;
+        VideoCaptureDevice device;
+        MotionDetector motionDetector;
+        double amountOfMotion = 0;
+        double lowerThresholdValue = 0.08;
+        double upperThresholdValue = 0.3;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnStop.Enabled = false;
+            motionDetector = new MotionDetector(new TwoFramesDifferenceDetector(), new MotionAreaHighlighting());
+            fic = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo item in fic)
+            {
+                comboBoxDevice.Items.Add(item.ToString());
+            }
+            comboBoxDevice.SelectedIndex = 0;
+            lowerThresholdBox.Text = lowerThresholdValue.ToString();
+            upperThresholdBox.Text = upperThresholdValue.ToString();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(fic[comboBoxDevice.SelectedIndex].MonikerString);
+            videoSourcePlayer1.VideoSource = device;
+            videoSourcePlayer1.Start();
+            btnStart.Enabled = false; btnStop.Enabled=true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer1.Stop();
+            amountOfMotion = 0;
+            btnStart.Enabled = true; btnStop.Enabled = false;
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            amountOfMotion = motionDetector.ProcessFrame(image);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (lowerThresholdValue <= amountOfMotion && amountOfMotion <= upperThresholdValue)
+                valueLabel.Text = "Amount of motion: " + amountOfMotion.ToString();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (lowerThresholdBox.Text.Length == 0 || upperThresholdBox.Text.Length == 0)
+            {
+                MessageBox.Show("You must specify the threshold range");
+            }
+            else
+            {
+                try
+                {
+                    double lowerThreshParsed = double.Parse(lowerThresholdBox.Text);
+                    double upperThreshParsed = double.Parse(upperThresholdBox.Text);
+                    if (lowerThreshParsed > upperThreshParsed)
+                    {
+                        MessageBox.Show("Lower threshold can't be bigger than the upper threshold");
+                    }
+                    else
+                    {
+                        lowerThresholdValue = lowerThreshParsed;
+                        upperThresholdValue = upperThreshParsed;
+                    }
+                }catch (Exception)
+                {
+                    MessageBox.Show("You must only enter digits in the threshold range");   
+                }
+            }
+        }
+    }
+}
