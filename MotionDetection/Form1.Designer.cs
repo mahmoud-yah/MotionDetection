b17aeec04be8e65a46b3b7753e4a3363aa59c364
Mahmoud Yahya <mahmoudyah.my@gmail.com>
2023-05-10 03:30:41 +0300

    Replaced the local camera with an RTSP camera.

--- MotionDetection/Form1.Designer.cs
+++ MotionDetection/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
-            this.comboBoxDevice = new System.Windows.Forms.ComboBox();
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.valueLabel = new System.Windows.Forms.Label();
@@ -40,29 +39,23 @@
             this.label2 = new System.Windows.Forms.Label();
             this.upperThresholdBox = new System.Windows.Forms.TextBox();
             this.saveButton = new System.Windows.Forms.Button();
+            this.cameraUrlTextBox = new System.Windows.Forms.TextBox();
+            this.cameraAddressLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // videoSourcePlayer1
             // 
-            this.videoSourcePlayer1.Location = new System.Drawing.Point(16, 43);
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(16, 54);
             this.videoSourcePlayer1.Name = "videoSourcePlayer1";
-            this.videoSourcePlayer1.Size = new System.Drawing.Size(497, 393);
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(497, 350);
             this.videoSourcePlayer1.TabIndex = 0;
             this.videoSourcePlayer1.Text = "videoSourcePlayer1";
             this.videoSourcePlayer1.VideoSource = null;
             this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
             // 
-            // comboBoxDevice
-            // 
-            this.comboBoxDevice.FormattingEnabled = true;
-            this.comboBoxDevice.Location = new System.Drawing.Point(13, 13);
-            this.comboBoxDevice.Name = "comboBoxDevice";
-            this.comboBoxDevice.Size = new System.Drawing.Size(279, 24);
-            this.comboBoxDevice.TabIndex = 1;
-            // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(325, 12);
+            this.btnStart.Location = new System.Drawing.Point(335, 12);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(87, 26);
             this.btnStart.TabIndex = 2;
@@ -83,7 +76,7 @@
             // valueLabel
             // 
             this.valueLabel.AutoSize = true;
-            this.valueLabel.Location = new System.Drawing.Point(13, 484);
+            this.valueLabel.Location = new System.Drawing.Point(13, 452);
             this.valueLabel.Name = "valueLabel";
             this.valueLabel.Size = new System.Drawing.Size(122, 16);
             this.valueLabel.TabIndex = 4;
@@ -98,7 +91,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 446);
+            this.label1.Location = new System.Drawing.Point(13, 414);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(179, 16);
             this.label1.TabIndex = 5;
@@ -106,7 +99,7 @@
             // 
             // lowerThresholdBox
             // 
-            this.lowerThresholdBox.Location = new System.Drawing.Point(236, 443);
+            this.lowerThresholdBox.Location = new System.Drawing.Point(236, 411);
             this.lowerThresholdBox.Name = "lowerThresholdBox";
             this.lowerThresholdBox.Size = new System.Drawing.Size(47, 22);
             this.lowerThresholdBox.TabIndex = 6;
@@ -114,7 +107,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(289, 446);
+            this.label2.Location = new System.Drawing.Point(289, 414);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 16);
             this.label2.TabIndex = 7;
@@ -122,14 +115,14 @@
             // 
             // upperThresholdBox
             // 
-            this.upperThresholdBox.Location = new System.Drawing.Point(325, 443);
+            this.upperThresholdBox.Location = new System.Drawing.Point(325, 411);
             this.upperThresholdBox.Name = "upperThresholdBox";
             this.upperThresholdBox.Size = new System.Drawing.Size(47, 22);
             this.upperThresholdBox.TabIndex = 8;
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(396, 442);
+            this.saveButton.Location = new System.Drawing.Point(396, 410);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(78, 24);
             this.saveButton.TabIndex = 9;
@@ -137,11 +130,29 @@
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
+            // cameraUrlTextBox
+            // 
+            this.cameraUrlTextBox.Location = new System.Drawing.Point(126, 14);
+            this.cameraUrlTextBox.Name = "cameraUrlTextBox";
+            this.cameraUrlTextBox.Size = new System.Drawing.Size(193, 22);
+            this.cameraUrlTextBox.TabIndex = 10;
+            // 
+            // cameraAddressLabel
+            // 
+            this.cameraAddressLabel.AutoSize = true;
+            this.cameraAddressLabel.Location = new System.Drawing.Point(16, 17);
+            this.cameraAddressLabel.Name = "cameraAddressLabel";
+            this.cameraAddressLabel.Size = new System.Drawing.Size(112, 16);
+            this.cameraAddressLabel.TabIndex = 11;
+            this.cameraAddressLabel.Text = "Camera Address:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 516);
+            this.ClientSize = new System.Drawing.Size(527, 481);
+            this.Controls.Add(this.cameraAddressLabel);
+            this.Controls.Add(this.cameraUrlTextBox);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.upperThresholdBox);
             this.Controls.Add(this.label2);
@@ -150,7 +161,6 @@
             this.Controls.Add(this.valueLabel);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.comboBoxDevice);
             this.Controls.Add(this.videoSourcePlayer1);
             this.Name = "Form1";
             this.Text = "Motion Detection";
@@ -163,7 +173,6 @@
         #endregion
 
         private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
-        private System.Windows.Forms.ComboBox comboBoxDevice;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Label valueLabel;
@@ -173,6 +182,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox upperThresholdBox;
         private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox cameraUrlTextBox;
+        private System.Windows.Forms.Label cameraAddressLabel;
     }
 }
 
--- MotionDetection/Form1.cs
+++ MotionDetection/Form1.cs
@@ -10,11 +10,14 @@ using System.Windows.Forms;
 using AForge.Video.DirectShow;
 using AForge.Imaging;
 using AForge.Vision.Motion;
+using AForge.Video;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        String cameraUrl;
+       
         public Form1()
         {
             InitializeComponent();
@@ -23,37 +26,50 @@ namespace WindowsFormsApp1
         FilterInfoCollection fic;
         VideoCaptureDevice device;
         MotionDetector motionDetector;
+        MJPEGStream videoStream;
         double amountOfMotion = 0;
-        double lowerThresholdValue = 0.08;
+        double lowerThresholdValue = 0.008;
         double upperThresholdValue = 0.3;
 
         private void Form1_Load(object sender, EventArgs e)
         {
             btnStop.Enabled = false;
             motionDetector = new MotionDetector(new TwoFramesDifferenceDetector(), new MotionAreaHighlighting());
-            fic = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            foreach (FilterInfo item in fic)
-            {
-                comboBoxDevice.Items.Add(item.ToString());
-            }
-            comboBoxDevice.SelectedIndex = 0;
             lowerThresholdBox.Text = lowerThresholdValue.ToString();
             upperThresholdBox.Text = upperThresholdValue.ToString();
+            
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            device = new VideoCaptureDevice(fic[comboBoxDevice.SelectedIndex].MonikerString);
-            videoSourcePlayer1.VideoSource = device;
-            videoSourcePlayer1.Start();
-            btnStart.Enabled = false; btnStop.Enabled=true;
+            if (cameraUrlTextBox.Text.Length == 0)
+            {
+                cameraUrl = "http://166.165.35.37:80/mjpg/video.mjpg";
+                MessageBox.Show("You didn't specify your camera IP address,\nyou will now be watching a random camera");
+            }
+            else
+            {
+                cameraUrl = cameraUrlTextBox.Text;
+            }
+            try
+            {
+                videoStream = new MJPEGStream(cameraUrl);
+                videoSourcePlayer1.VideoSource = new AsyncVideoSource(videoStream);
+                videoSourcePlayer1.Start();
+                btnStart.Enabled = false; btnStop.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnStop_Click(object sender, EventArgs e)
         {
+            //amountOfMotion = 0;
             videoSourcePlayer1.Stop();
-            amountOfMotion = 0;
             btnStart.Enabled = true; btnStop.Enabled = false;
+            valueLabel.Text = "Amount of motion: 0";
         }
 
         private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
@@ -64,7 +80,10 @@ namespace WindowsFormsApp1
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (lowerThresholdValue <= amountOfMotion && amountOfMotion <= upperThresholdValue)
+            {
+                //do whatever you want!!
                 valueLabel.Text = "Amount of motion: " + amountOfMotion.ToString();
+            }
         }
 
         private void saveButton_Click(object sender, EventArgs e)
@@ -79,6 +98,7 @@ namespace WindowsFormsApp1
                 {
                     double lowerThreshParsed = double.Parse(lowerThresholdBox.Text);
                     double upperThreshParsed = double.Parse(upperThresholdBox.Text);
+
                     if (lowerThreshParsed > upperThreshParsed)
                     {
                         MessageBox.Show("Lower threshold can't be bigger than the upper threshold");
@@ -90,7 +110,7 @@ namespace WindowsFormsApp1
                     }
                 }catch (Exception)
                 {
-                    MessageBox.Show("You must only enter digits in the threshold range");   
+                    MessageBox.Show("You should only enter digits in the threshold range");   
                 }
             }
         }
